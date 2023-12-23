58ed895f5477cd49a35a1c8be7881df4ca49fa3d
DongNT <donganton5@gmail.com>
2023-12-24 01:28:38 +0700

    update

--- 05-UI-phoneShop.py
+++ 05-UI-phoneShop.py
@@ -7,24 +7,77 @@ from PIL import Image, ImageOps, ImageDraw
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
+        # Specify the path to your JSON file
+        json_file_path = 'dataPhone.json'
+        # Open the JSON file and load its contents into a Python dictionary
+        with open(json_file_path, 'r') as file:
+            python_data = json.load(file)
+        resizable = False
         self.title("Phone Shop")
-        self.geometry("800x800")
-        self.resizable(width=False, height=False)
-        # self.grid_columnconfigure(0, weight=1)
-        # self.grid_rowconfigure(0, weight=1)
+        self.geometry("1500x800")
+        self.resizable(width=resizable, height=resizable)
+        # Configure both rows and columns to expand with the window size
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        # label_name = customtkinter.CTkLabel(self, text="Phone Name:")
-        # label_name.grid(row=0, column=0, padx=10, pady=5)
+        # # Create a frame for the scroll layout
+        frame_width_percentage = 90
+        frame_width = self.winfo_screenwidth() * frame_width_percentage / 100
+        frame = customtkinter.CTkScrollableFrame(
+            self, fg_color="gray", width=frame_width)
+
+        # Center the frame
+        frame.grid(row=0, column=0, padx=(
+            self.winfo_screenwidth() - frame_width) / 2, sticky="nswe")
+
+        # Configure the column to take up the desired percentage of the window width
+        self.grid_columnconfigure(0, weight=1)
+
+        num_columns = 4
+        num_rows = len(python_data) // num_columns + \
+            (len(python_data) % num_columns > 0)
+
+        # Render phones
+        for index in range(len(python_data)):
+            iRow, iCol = divmod(index, num_columns)
+            phone_data = python_data[index]
+            phone_card = PhoneCard(frame, **phone_data)
+            phone_card.grid(row=iRow, column=iCol, padx=5, pady=5)
+
+
+class PhoneCard(customtkinter.CTkFrame):
+    def __init__(self, master, id, name, price, category):
+        # import image
         image_folder = 'img'
-
         image_path = os.path.join(image_folder, "iphone1.png")
-        rounded_image = add_border_radius(image_path, 20)
+        rounded_image = add_border_radius(image_path, 30)
         my_image = customtkinter.CTkImage(light_image=rounded_image,
                                           dark_image=rounded_image,
                                           size=(300, 300))
-        image_label = customtkinter.CTkLabel(self, image=my_image, text="")
-        image_label.pack()
+
+        super().__init__(master)
+        self.grid(sticky='nswe')
+        self.grid_columnconfigure(0, weight=1)
+
+        # Create widgets for the PhoneCard frame
+        image_label = customtkinter.CTkLabel(
+            self, image=my_image, text='')
+        image_label.grid(row=0, column=0, padx=5, pady=5, sticky="s")
+        name_label = customtkinter.CTkLabel(self, text=f"Name: {name}")
+        name_label.grid(row=1, column=0, padx=5, pady=5, sticky="s")
+
+        price_label = customtkinter.CTkLabel(self, text=f"Price: {price}")
+        price_label.grid(row=2, column=0, padx=5, pady=5, sticky="s")
+
+        category_label = customtkinter.CTkLabel(
+            self, text=f"Category: {category}")
+        category_label.grid(row=3, column=0, padx=5, pady=5, sticky="s")
+        buttonThemGioHang = customtkinter.CTkButton(
+            self, text="Mua luôn", command=lambda: self.themVaoGioHang(id))
+        buttonThemGioHang.grid(row=4, column=0, padx=5, pady=5, sticky="s")
+
+    def themVaoGioHang(self, id):
+        print(f"Đã mua {id}")
 
 
 def add_border_radius(image_path, radius):
@@ -40,9 +93,3 @@ def add_border_radius(image_path, radius):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-    # Specify the path to your JSON file
-    json_file_path = 'dataPhone.json'
-    # Open the JSON file and load its contents into a Python dictionary
-    with open(json_file_path, 'r') as file:
-        python_data = json.load(file)
